837b288bce8544cfd6d46d76143768c3ac702983
James Suescun <james.steven2318@gmail.com>
2022-03-30 22:30:02 -0400

    App updates with cryptocurrency values

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Currencies from './pages/Currencies.js';
-import Main from './pages/main';
+import Nav from './components/Nav.js'
+import Main from './pages/Main.js';
 import Price from './pages/Price.js'
 
 function App() {
   
   return (
     <div className='App'>
+      <Nav />
       <Routes>
         <Route path='/' element={ <Main /> } />
         <Route path='/currencies' element={ <Currencies /> } />
-        <Route path='/price' element={ <Price /> } />
+        <Route path='/price/:symbol' element={ <Price /> } />
       </Routes>
     </div>
     
---
+++ src/pages/Price.js
@@ -0,0 +1,49 @@
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+
+export default function Price (props) {
+
+  const apiKey = 'D44C9DE8-B752-4409-A654-BB95BC4C19DB';
+  const params = useParams()
+  const symbol = params.symbol
+  const url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${ symbol }/USD?apikey=${ apiKey }`;
+
+  const [coin, setCoin] = useState( null );
+
+  const getCoin = async () => {
+    try{
+      const response = await fetch( url );
+      const data = await response.json();
+      setCoin( data );
+    } 
+    
+    catch( error ) {
+      console.error( error )
+    }
+  };
+
+  // useEffect to run getCoin when component mounts
+  useEffect(() => {
+    getCoin();
+  }, []);
+
+  // loaded function for when data is fetched
+  const loaded = () => {
+    return (
+      <div>
+        <h1>
+          { coin.asset_id_base }/{ coin.asset_id_quote }
+        </h1>
+        <h2>{ coin.rate }</h2>
+      </div>
+    );
+  };
+
+  // Function for when data doesn't exist
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  // if coin has data, run the loaded function, otherwise, run loading
+  return coin && coin.rate ? loaded() : loading();
+};
